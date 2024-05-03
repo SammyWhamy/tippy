c3e9d5b810081a52d0047e3d050fcfe6d20a881f
aether <71261542+aetherpw@users.noreply.github.com>
2024-05-03 17:04:39 +0000

    chore: styling

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -1,18 +1,18 @@
-import * as z from 'zod';
+import * as z from "zod";
 
 export const envie = z.object({
     DISCORD_TOKEN: z.string(),
-    REDIS_HOST: z.string().default('localhost'),
+    REDIS_HOST: z.string().default("localhost"),
     REDIS_PORT: z.number().default(6379),
-    REDIS_PASSWORD: z.string().default(''),
+    REDIS_PASSWORD: z.string().default(""),
     REDIS_DATABASE: z.number().default(0),
     SHARD_COUNT: z.number(),
     SHARDS_PER_WORKER: z.number(),
     METRICS_PORT: z.number().default(9090),
-    METRICS_HOST: z.string().default('0.0.0.0'),
+    METRICS_HOST: z.string().default("0.0.0.0"),
     HOSTNAME: z.string(),
     LOG_LEVEL: z.number().default(3),
-    DOCKER_SOCK: z.string().default('/var/run/docker.sock'),
+    DOCKER_SOCK: z.string().default("/var/run/docker.sock"),
 });
 
 export type Environment = z.infer<typeof envie>;
